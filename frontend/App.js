1b98d59b6ddb6e4a6e5fde3f11874c12484f78b0
Clare Zhu <clare.zhu@mail.utoronto.ca>
2024-03-08 20:49:16 -0500

    added components folder

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import index from './app/Homepage';
+import index from './app/pages/Homepage';
 
 export default function App() {
   return (
