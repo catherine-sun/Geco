90ea24b77c38d8eb9c60750130e88200f08071ac
Penguinfami <christine.spyphone.zhao@gmail.com>
2024-03-08 21:28:12 -0500

    Added get and set functions for storage

---
+++ frontend/app/components/tools/db.js
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeData = async (
+    key, 
+    item, 
+    onError = () => {}
+) => {
+    try {
+        await AsyncStorage.setItem(
+            key,
+            item,
+            onError
+        )
+    } catch (e) {
+        onError();
+    }
+}
+
+export const getData = async (
+    key, 
+    onError = () => {}
+) => {
+    try {
+        const item = await AsyncStorage.getItem(key, onError)
+        return item;
+    } catch (e) {
+        onError();
+    }
+}
