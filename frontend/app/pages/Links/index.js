25abda6e92c2fe44f567870c4adebe7d6e218c1f
Clare Zhu <clare.zhu@mail.utoronto.ca>
2024-03-09 21:46:38 -0500

    changed link page styling

--- frontend/app/pages/Links/index.js
+++ frontend/app/pages/Links/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, SafeAreaView, Button} from 'react-native';
+import { Text, SafeAreaView, Pressable} from 'react-native';
 import { openBrowserAsync} from 'expo-web-browser';
 
 import DisplayButtons from '../../components/DisplayButtons';
@@ -13,17 +13,32 @@ function Links() {
       <Text style={styles.titleStyle}>
         Minimum effort, links only
       </Text>
-      <Text style={styles.textStyle}>
-        https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-blue-bin/
-      </Text>
-      <Text style={styles.textStyle}>
-      https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-green-bin/
-      </Text>
 
-      <Button title='Recyclables'
-        onPress={() => openBrowserAsync('https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-blue-bin/')} />
-      <Button title='Compost'
-        onPress={() => openBrowserAsync('https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-green-bin/')} />
+      <SafeAreaView style={styles.pressView}>
+        <Text style={styles.header} >Here are a few useful links!</Text>
+        <Pressable style={styles.pressStyle}
+          onPress={() => openBrowserAsync('https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-garbage-bin/')}>
+          <Text style={styles.textStyle} >What Belongs in the Garbage?</Text>
+        </Pressable>
+        <Pressable style={styles.pressStyle}
+          onPress={() => openBrowserAsync('https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-blue-bin/')}>
+          <Text style={styles.textStyle} >What Belongs in the Recycling?</Text>
+        </Pressable>
+        <Pressable style={styles.pressStyle}
+          onPress={() => openBrowserAsync('https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/what-goes-in-my-green-bin/')} >
+          <Text style={styles.textStyle} >What Belongs in the Compost?</Text>
+        </Pressable>
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.pressView}>
+        <Text style={styles.header} >Want to know where an item should go? Ask the Waste Wizard!</Text>
+        <Pressable style={styles.pressStyle}
+          onPress={() => openBrowserAsync('https://www.toronto.ca/services-payments/recycling-organics-garbage/waste-wizard/')}>
+          <Text style={styles.textStyle} >Toronto's Waste Wizard</Text>
+        </Pressable>
+      </SafeAreaView>
+
+
 
 
       <DisplayButtons currentPath='Links' />
